037e35da0deb76522d8b67091235490f0c27a798
Matthew Williams <32585844+mw3244@users.noreply.github.com>
2018-11-05 16:06:47 -0800

    Added more parser elements

--- gameFunctions.py
+++ gameFunctions.py
@@ -4,14 +4,13 @@ import sys
 class GameFunctions:
     def __init__(self, finished):
         self.finished = finished
+        self.image_lib = [pygame.image.load('images/bg.png'), pygame.image.load('images/background/floor.png'),
+                     pygame.image.load('images/mario/mario_small_idle.png'), pygame.image.load('images/background/tile.png'),
+                          pygame.image.load('images/background/brick.png'), pygame.image.load('images/enemies/goomba.png'),
+                          pygame.image.load('images/enemies/koopa0.png')]
 
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
-    def load_image_library(self):
-        image_lib = [pygame.image.load('images/bg.png'), pygame.image.load('images/background/floor.png'), pygame.image.load('images/mario/mario_small_idle.png')]
-
-        return image_lib
 
--- map.py
+++ map.py
@@ -33,15 +33,38 @@ class Map:
         self.brick_image = pygame.transform.scale(self.brick_image, (settings.tileSize, settings.tileSize))
         self.brick_rect = self.brick_image.get_rect()
 
+        self.solids = []
+        self.solid_image = self.image_library[3].convert_alpha()
+        self.solid_image = pygame.transform.scale(self.solid_image, (settings.tileSize, settings.tileSize))
+        self.solid_rect = self.solid_image.get_rect()
+
+        self.blocks = []
+        self.blocks_value = []
+        self.block_image = self.image_library[4].convert_alpha()
+        self.block_image = pygame.transform.scale(self.block_image, (settings.tileSize, settings.tileSize))
+        self.block_rect = self.block_image.get_rect()
+
         self.mario_image = self.image_library[2].convert_alpha()
         self.mario_image = pygame.transform.scale(self.mario_image, (settings.tileSize, settings.tileSize))
         self.mario_rect = self.mario_image.get_rect()
-
-
-
-        self.deltaX = self.deltaY = settings.tileSize
         self.marioXInit = 0
         self.marioYInit = 0
+
+        self.goombas = []
+        self.goombasXInit = []
+        self.goombasYInit = []
+        self.goomba_image = self.image_library[5].convert_alpha()
+        self.goomba_image = pygame.transform.scale(self.goomba_image, (settings.tileSize, settings.tileSize))
+        self.goomba_rect = self.goomba_image.get_rect()
+
+        self.koopas = []
+        self.koopasXInit = []
+        self.koopasYInit = []
+        self.koopa_image = self.image_library[6].convert_alpha()
+        self.koopa_image = pygame.transform.scale(self.koopa_image, (settings.tileSize, settings.tileSize))
+        self.koopa_rect = self.koopa_image.get_rect()
+
+        self.deltaX = self.deltaY = settings.tileSize
         self.alternate_parse_file()
 
     def alternate_parse_file(self):
@@ -55,10 +78,34 @@ class Map:
                 col = row[nCol]
                 if col == 'x':
                     self.bricks.append(pygame.Rect(nCol * dx, nRow * dy, w, h))
-                if col == 'm':
+                elif col == 'm':
                     self.mario_rect = pygame.Rect(nCol * dx, nRow * dy, w, h)
                     self.marioXInit = self.mario_rect.x
                     self.marioYInit = self.mario_rect.y
+                elif col == 's':
+                    self.solids.append(pygame.Rect(nCol * dx, nRow * dy, w, h))
+                elif col == 'b' or col == '1' or col == '9' or col == 'p' or col == 'S' or col == 'l':
+                    self.blocks.append(pygame.Rect(nCol * dx, nRow * dy, w, h))
+                    if col == 'b':
+                        self.blocks_value.append("None")
+                    elif col == '1':
+                        self.blocks_value.append("1Coin")
+                    elif col == '9':
+                        self.blocks_value.append("9Coin")
+                    elif col == 'p':
+                        self.blocks_value.append("Powerup")
+                    elif col == 'S':
+                        self.blocks_value.append("Starman")
+                    elif col == 'l':
+                        self.blocks_value.append("Lifeup")
+                elif col == 'g':
+                    self.goombas.append(pygame.Rect(nCol * dx, nRow * dy, w, h))
+                    self.goombasXInit.append(nCol * dx)
+                    self.goombasYInit.append(nRow * dy)
+                elif col == 'k':
+                    self.koopas.append(pygame.Rect(nCol * dx, nRow * dy, w, h))
+                    self.koopasXInit.append(nCol * dx)
+                    self.koopasYInit.append(nRow * dy)
 
 
     # def parse_file(self):
@@ -111,4 +158,12 @@ class Map:
     def blitme(self):
         for rect in self.bricks:
             self.screen.blit(self.brick_image, rect)
+        for rect in self.solids:
+            self.screen.blit(self.solid_image, rect)
+        for rect in self.blocks:
+            self.screen.blit(self.block_image, rect)
         self.screen.blit(self.mario_image, self.mario_rect)
+        for rect in self.goombas:
+            self.screen.blit(self.goomba_image, rect)
+        for rect in self.koopas:
+            self.screen.blit(self.koopa_image, rect)
