46ca4474561f5e55c35bc07f64909af399435e80
Mbraun5 <braun.matthewwork@gmail.com>
2018-11-12 17:44:56 -0800

    Re-added deleted code

---
+++ coin.py
@@ -0,0 +1,33 @@
+class CoinBase:
+    def __init__(self, image_library, rect, screen, settings):
+        self.screen = screen
+        self.settings = settings
+        self.img_lib = image_library
+
+        self.img = self.img_lib[0]
+        self.rect = rect
+
+    def blit(self):
+        if self.rect.right < 0 or self.rect.left > self.settings.screenWidth:
+            return
+        self.screen.blit(self.img, self.rect)
+
+    def update_x_coor(self, speed):
+        self.rect.x += speed
+
+
+class Coin(CoinBase):
+    def __init__(self, image_library, rect, screen, settings):
+        super().__init__(image_library, rect, screen, settings)
+        self.index = 0
+        self.indexInc = 1
+
+    def blit(self):
+        if self.rect.right < 0 or self.rect.left > self.settings.screenWidth:
+            return
+        self.screen.blit(self.img_lib[self.index], self.rect)
+
+    def change_index(self):
+        self.index += self.indexInc
+        if self.index == 0 or self.index == 2:
+            self.indexInc *= -1
---
+++ goomba.py
@@ -0,0 +1,64 @@
+from mario import Mario
+import pygame
+from pygame.sprite import Sprite
+from map import Map
+
+
+class Goomba(Sprite):
+    def __init__(self,  screen, settings, rect, imglib):
+        super(Goomba, self).__init__()
+        self.map = map
+        self.screen = screen
+        self.speed_factor = 1  # -1 left +1 right
+        self.settings = settings
+        # self.y_mod = 200
+        self.invincible = False
+        self.direction_left = True
+
+        self.images = []
+        self.images.append(imglib[0])
+        self.images.append(pygame.transform.flip(imglib[0], True, False))
+        self.images.append(imglib[1])
+        self.image = self.images[0]
+        self.rect = rect
+        self.screen_rect = screen.get_rect()
+
+        self.rect.x = rect.x
+        self.rect.y = rect.y
+
+        # Store a decimal value for the ship's center
+        self.center_x = float(self.rect.x)
+        self.center_y = float(self.rect.y)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = True
+        # self.moving_up = False
+        # self.moving_down = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center_x += self.speed_factor
+        elif self.moving_left and self.rect.left > 0:
+            self.center_x -= self.speed_factor
+
+        # Update rect object from self.center
+        self.rect.x = self.center_x
+        self.rect.y = self.center_y
+
+    def flip_img(self):
+        if self.direction_left:
+            self.image = self.images[1]
+        else:
+            self.image = self.images[0]
+        self.direction_left = not self.direction_left
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+        if self.rect.right < 0 or self.rect.left > self.settings.screenWidth:
+            return
+        self.screen.blit(self.image, self.rect)
+
+    def change_direction(self):
+        self.moving_left = not self.moving_left
+        self.moving_right = not self.moving_right
